ba28d073efed6467cb69d847751713bad3b28d9f
fizbanstreasuryofdragons <167873534+fizbanstreasuryofdragons@users.noreply.github.com>
2024-05-06 15:39:01 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 basic.forever(function () {
     if (input.isGesture(Gesture.Shake)) {
-        basic.showNumber(randint(0, 7))
+        basic.showNumber(randint(1, 6))
         basic.pause(2000)
         basic.showLeds(`
             . . . . .
